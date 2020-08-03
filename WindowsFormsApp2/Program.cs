f08d27f41fe79810cc258fd389e2e326a20c81d9
由幸 大坪 <eeeelllleeee24@gmail.com>
2020-08-03 23:53:24 +0900

    コミット

--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApp2
         [STAThread]
         static void Main()
         {
-            Application.EnableVisu alStyles();
+            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }

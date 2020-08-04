08a4862942da92f9063e40792d88e571bb094fc9
由幸 大坪 <eeeelllleeee24@gmail.com>
2020-08-04 23:25:52 +0900

    TOP画面を編集

--- WindowsFormsApp2/UserSaveForm.Designer.cs
+++ WindowsFormsApp2/UserSaveForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp2
 {
-    partial class Form1
+    partial class UserSaveForm
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UserSaveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "UserSaveForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ユーザー登録";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
